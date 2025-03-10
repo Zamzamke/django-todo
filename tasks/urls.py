0ea9ca096a96ebb968a46dc41318eca9a658b9b8
Zamzamke <zamzamchelimo@gmail.com>
2025-03-10 22:38:08 +0300

    Initial commit: Django project setup

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import task_list, task_update, task_delete
+
+urlpatterns = [
+    path('', task_list, name='task_list'),
+    path('edit/<int:task_id>/', task_update, name='task_update'),
+    path('delete/<int:task_id>/', task_delete, name='task_delete'),
+]
