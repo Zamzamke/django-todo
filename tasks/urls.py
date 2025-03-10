32287c27829348de99ede77d73a282e6faf8bfb1
Zamzamke <zamzamchelimo@gmail.com>
2025-03-11 00:23:23 +0300

    add bootstrap to the templates to make it visually appealing

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,5 @@ from .views import task_list, task_update, task_delete
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('edit/<int:task_id>/', task_update, name='task_update'),
-    path('delete/<int:task_id>/', task_delete, name='task_delete'),
+    path('delete/<int:task_id>/', task_delete, name='task_delete')
 ]
