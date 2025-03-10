d6af7a6ca510e2f2403210ec40a28c6bb61da87b
Zamzamke <zamzamchelimo@gmail.com>
2025-03-10 23:46:55 +0300

    update profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 def user_register(request):
     if request.method == "POST":
@@ -19,10 +21,14 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')   
+            return redirect(reverse('profile')) 
         else:
             return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
     return render(request, 'accounts/login.html')
+
+@login_required
+def user_profile(request):
+    return render(request, 'accounts/profile.html', {'user': request.user})
 
 def user_logout(request):
     logout(request)
